4925dbb5ee913edad636186b9af658f7893d0efc
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2026-01-14 16:01:45 +0100

    style: linted and formatted all files

--- lib/shared/utils/platform_resolver.dart
+++ lib/shared/utils/platform_resolver.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 
 bool isCupertino(BuildContext context) {
   final platform = Theme.of(context).platform;
-  return platform == TargetPlatform.iOS ||
-         platform == TargetPlatform.macOS;
+  return platform == TargetPlatform.iOS || platform == TargetPlatform.macOS;
 }
